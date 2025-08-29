db57fd2bda68ff26cecf17ce28289e323ffb6fad
Lord CJ Alon UCM <lordcjalon03@gmail.com>
2025-08-29 17:59:44 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,11 @@
+package Chapter_1;
+// the single line
+public class Comments  
+{
+    public static void main(String[] args)
+    {
+        /* the mutli-line */
+        System.out.println("Hi, this is comment");
+         /** the documentation */
+    }
+}                   
---
+++ Chapter_1/FavorateSong.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+
+ public class FavorateSong 
+ {
+ public static void main(String [] args) 
+ {
+ 
+   System.out.println("But I apologize for coming into your life");
+   System.out.println("Just to break your heart to pieces, and then leave you in the night");
+   System.out.println("And I tried my best to stay by your side ");
+   System.out.println("So, goodbye to the ones we once loved");
+   
+ }
+ }
