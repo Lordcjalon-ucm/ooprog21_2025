555a862f3a4b5e36d57e707c767fb66bc2f6804a
Lord <lordcjalon03@gmail.com>
2025-09-08 22:42:58 +0800

    Signed-off-by: Lord <lordcjalon03@gmail.com>

--- Chapter_2/UsingJOptionPaneDialogs.java
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -1,37 +1,13 @@
 import javax.swing.JOptionPane;
 
-public class UsingJOPtionPaneDialog {
+public class UsingJOptionPaneDialogs {
     public static void main(String[] args) {
-        String name;
-        int confirm;
-
-        do {
-            
-            name = JOptionPane.showInputDialog(null, "Enter your name:", "Name Input", JOptionPane.QUESTION_MESSAGE);
-
-           
-            if (name == null) {
-                int exit = JOptionPane.showConfirmDialog(null, "Do you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION);
-                if (exit == JOptionPane.YES_OPTION) {
-                    System.exit(0); 
-                } else {
-                    continue; 
-                }
-            }
-
-            
-            confirm = JOptionPane.showConfirmDialog(null, "Do you want to display your name?", "Confirmation", JOptionPane.YES_NO_CANCEL_OPTION);
-
-            
-            if (confirm != JOptionPane.YES_OPTION) {
-                continue; 
-            }
-
-            
-            JOptionPane.showMessageDialog(null, "Your name is: " + name, "Name Display", JOptionPane.INFORMATION_MESSAGE);
-
-        } while (confirm != JOptionPane.YES_OPTION); 
+        
+        String name = JOptionPane.showInputDialog(null, "What is your name?", "Input", JOptionPane.QUESTION_MESSAGE);
 
         
+        if (name != null) {
+            JOptionPane.showMessageDialog(null, "Hello, " + name + "!", "Message", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
